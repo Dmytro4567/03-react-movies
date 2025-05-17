1d3325d5142126dc66e735d2b34b68d6d727a529
Dmytro <dmitry.dvernikov@gmail.com>
2025-05-17 16:38:33 +0300

    Add the basic project structure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import css from './App.module.css'
+import {useState} from 'react'
+
+
+function App() {
+
+    return (
+        <>
+            <h1>Hello, this is my component!</h1>
+        </>
+    )
+}
+
+export default App
